ecc19e293d43a9cb5fdfd0695fe212567dde7f6f
Wickramaarachchi W.A.M.N <it18014150@my.sliit.lk>
2021-08-24 16:30:32 +0530

    Workers profile UI create (Workers).

--- app/src/main/java/com/example/maddfinalproject/MainActivity.kt
+++ app/src/main/java/com/example/maddfinalproject/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.maddfinalproject
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val btnProfile = findViewById<Button>(R.id.btnProfile)
+//        btnProfile.setOnClickListener {
+//            val intent = Intent(this, works::class.java)
+//            works(intent)
+//        }
     }
 }
---
+++ app/src/main/java/com/example/maddfinalproject/works.kt
@@ -0,0 +1,12 @@
+package com.example.maddfinalproject
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class works(intent: Intent) : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_works)
+    }
+}
