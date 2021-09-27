93aa9f1330cf9e2e7544c467a8ddb18289d30198
Wickramaarachchi W.A.M.N <it18014150@my.sliit.lk>
2021-09-27 17:28:32 +0530

    UI design.

---
+++ app/src/main/java/com/example/maddfinalproject/DesignerMainUI.kt
@@ -0,0 +1,11 @@
+package com.example.maddfinalproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DesignerMainUI : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_designer_main_ui)
+    }
+}
---
+++ app/src/main/java/com/example/maddfinalproject/Portfolio.kt
@@ -0,0 +1,11 @@
+package com.example.maddfinalproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Portfolio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_portfolio)
+    }
+}
